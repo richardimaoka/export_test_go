efe2795307ead02817e9194ccb1a50c53635ac3c
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-31 23:58:11 +0900

    this is not working

--- sub_test/sublib_test.go
+++ sub_test/sublib_test.go
@@ -1,7 +1,14 @@
 package sublib
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/richardimaoka/export_test_go/sub"
+)
 
 func TestSublib(t *testing.T) {
+	sub.ExportedAdd(1, 2)
+	sub.ExportedAdd(1, 2)
+	sub.ReExportedAdd(1, 2)
 	t.Logf("TestSublib")
 }
